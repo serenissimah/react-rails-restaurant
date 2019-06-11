013459d299fffc900ff0fcda999b97a23e0ce5c7
Serena Hong <serenahong.usa@gmail.com>
2019-06-10 20:14:26 -0600

    rails done

---
+++ app/controllers/api/items_controller.rb
@@ -0,0 +1,42 @@
+class Api::ItemsController < ApplicationController
+  before_action :set_menu
+  before_action :set_item, only: [:update, :destroy]
+  
+    def index
+      render json: @menu.items
+    end
+  
+    def create
+      item = Item.new(item_params)
+      if item.save
+        render json: item
+      else
+        render json: { errors: item.errors }, status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      @item.update(item_params)
+      render json: @item
+    end
+  
+    def destroy
+      @item.destroy
+      render json: { message: "Item deleted"}
+    end
+  
+    private
+    def item_params
+      params.require(:item).permit(:name)
+    end
+  
+    def set_item
+      @item = Item.find(params[:id])
+    end
+  
+    def set_menu
+      @menu = Menu.find(params[:menu_id])
+    end
+  
+  
+  end
